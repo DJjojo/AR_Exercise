d7f3a43a660dbb7473805b05010dbee2c5766c8c
DJjojo <jojo.runkel@freenet.de>
2016-04-20 16:13:05 +0200

    Added Exercise_1 dir

---
+++ Exercise_1/Exercise_1/Test_01.cpp
@@ -0,0 +1,6 @@
+#include <opencv2\opencv.hpp>
+#include <cstdio>
+
+void main(){
+	std::cout << "VERSON NUMBER: " << CV_VERSION << std::endl;
+}
---
+++ Exercise_1/Exercise_1/Test_1.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+#include <opencv2\opencv.hpp>
+
+void main()
+{
+	std::cout << "OpenCV Version: " << CV_VERSION << std::endl;
+}
