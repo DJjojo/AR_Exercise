9771e7a0d72ce10f52434df3d5892332135d51bf
ga38lun <ga38lun@mytum.de>
2016-04-26 16:45:07 +0200

    Improvements;cleanup into sheet 1 and 2

--- Exercise_1/Exercise_1/Sheet_01.cpp
+++ Exercise_1/Exercise_1/Sheet_01.cpp
@@ -12,6 +12,7 @@ using namespace std;
 void main()
 {
 	namedWindow("Sheet_01");
+	namedWindow("Sheet_02");
 	VideoCapture cap(0);
 	if (!cap.isOpened()){
 		printf("NOT OPENED");
@@ -19,23 +20,36 @@ void main()
 	}
 	int th = 0;
 	bool adp = false;
-	for (;;){
-		Mat frame;
-		cap >> frame;
-		cvtColor(frame, frame, CV_BGR2GRAY);
+	int pixN = 0;
+	int c = 51;
 
-		
-		
+	for (;;){
+		Mat frame_01,frame_02;
+
+		//SHEET_01
+		cap >> frame_01;
+		cvtColor(frame_01, frame_01, CV_BGR2GRAY);
 		if (waitKey(30) == 't')adp = !adp;
 		createTrackbar("threshold", "Sheet_01", &th, 256);
-
 		if (!adp){
-			threshold(frame, frame, (double)(th), 128, THRESH_BINARY);
+			threshold(frame_01, frame_01, (double)(th), 128, THRESH_BINARY);
 		}else{
-			adaptiveThreshold(frame, frame, (double)(th), ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 3, 1);
-		}
+			createTrackbar("pixelNeighborhood", "Sheet_01", &pixN, 100);
+			createTrackbar("constant", "Sheet_01", &c, 100);
 
-		imshow("Sheet_01", frame);
+			adaptiveThreshold(frame_01, frame_01, (double)(th), ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 3+2*pixN, c-50);
+		}
+		imshow("Sheet_01", frame_01);
+		
+		//SHEET_02
+		cap >> frame_02;
+		cvtColor(frame_02, frame_02, CV_BGR2GRAY);
+		adaptiveThreshold(frame_02, frame_02, 120, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 13, -26);
+		
+		
+		imshow("Sheet_02", frame_02);
+
+
 		if (waitKey(30) == 27)break;
 	}
 
